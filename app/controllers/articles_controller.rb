9bd3359fc5e5b8d0c17b682b966ed3829df04dd5
Loïc BERTHOU <loicberthou@gmail.com>
2015-12-29 18:21:13 +0000

    Implement show article feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,10 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
 
   def new
     @article = Article.new
